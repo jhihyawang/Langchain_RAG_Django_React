182f6a980b8d56b960cc8ff629307f1b6944841f
jhihyawang <jhihyawang@smail.nchu.edu.tw>
2025-04-21 05:31:04 +0800

    no message

--- backend/common/module/ai/model/i_model.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class IModel(ABC):
-    @abstractmethod
-    def generate(self, query: str, temperature=0.8, max_token=2048, top_p=0.1) -> str:
-        pass
